b3461236c4b9338cc83eff5f58426b09cbd6b8d1
Mantas <mantas7776@gmail.com>
2019-05-28 20:33:13 +0300

    file handle added

---
+++ UniVM/Handles/FileHandle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniVM
+{
+    class FileHandle : Handle
+    {
+        private StorageFile file;
+        int seek = 0;
+
+        public FileHandle(StorageFile file)
+        {
+            this.file = file;
+        }
+
+        public override byte read()
+        {
+            return file[seek++];
+        }
+
+        public override bool write(byte c)
+        {
+            file[seek++] = c;
+            return true;
+        }
+    }
+}
