2083fafacd6464fe620b5f3d19b3f828f78cfa11
Virtouzo-WorkPC <rokastulaba@gmail.com>
2019-06-01 04:16:07 +0300

    das

--- UniVM/Main.cs
+++ UniVM/Main.cs
@@ -7,40 +7,10 @@ namespace UniVM
     class MainWrapper {
         public static void Main(string[] args) {
 
-            //RealMachine realMachine = new RealMachine();
-            //var randomStorage = new Storage("test.bin");
-            //byte[] altcode = Util.getCode("MOVA 1\nMOVB 2\nADD\nSUB\nHALT\n");
-            //byte[] altdata = Util.getData("FFFFFFFFAAAABBBB66696C652E74787400000000");
-            //RealMachine realMachine = new RealMachine();
-            //var randomStorage = new Storage("test.bin");
-            //byte[] altcode = Util.getCode("MOVA 1\nMOVB 2\nADD\nSUB\nHALT\n");
-            //byte[] altdata = Util.getData("FFFFFFFFAAAABBBB");
-            //RealMachine realMachine = new RealMachine();
-            //var randomStorage = new Storage("test.bin");
-            //byte[] altcode = Util.getCode("MOVA 12\nnADD\nSUB\nHALT\n");
-            //byte[] altdata = Util.getData("FFFFFFFFAAAABBBB\"big\0\"00000000");
-
-            //VMInfo codeInfo = new VMInfo() { code = altcode, data = altdata };
-            //int size = Util.getProgramSizeInFile(codeInfo);
-            //StorageFile.DeleteFile(randomStorage, "program1");
-            //var file = StorageFile.createFile(randomStorage, "program1", Util.getProgramSizeInFile(codeInfo));
-            //var codeFile = StorageFile.Open(randomStorage, "program1");
-            //Util.saveCodeToFile(codeFile, codeInfo);
-            //VMInfo info = Util.readCodeFromFile(codeFile);
-            //realMachine.start();
-            //var k = new Kernel();
-
-            //var randomStorage = new Storage("test.bin");
-            //byte[] altcode = Util.getCode("MOVA 1\nMOVB 2\nADD\nSUB\nHALT\n");
-            //byte[] altdata = Util.getData("FFFFFFFFAAAABBBB66696C652E74787400000000");
-            //VMInfo codeInfo = new VMInfo() { code = altcode, data = altdata };
-            //var codeFile = StorageFile.Open(randomStorage, "program1");
-            //Util.saveCodeToFile(codeFile, codeInfo);
-
             Application.EnableVisualStyles();
-            //Application.Run(new RMMain());
+            Application.Run(new RMMain());
             
-            Application.Run(new OSMain());
+            //Application.Run(new OSMain());
             //Kernel kernel = new Kernel();
             //while (true)
             //{
